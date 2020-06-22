20110e9da902fc63a8644305aa1026e8b3793388
John Michael Aro <mikoaro1212@gmail.com>
2020-06-23 00:08:24 +0800

    Change channel

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,14 +8,16 @@ declare var Ably: any;
 })
 export class AppComponent implements OnInit {
   title = 'bolton-test';
+  data = [];
 
   ably = new Ably.Realtime('ZoVHGw.4kc1eQ:xNiIHH2-oeOYIOwG');
-  channelName = '[product:ably-tfl/tube]tube:disruptions';
+  channelName = '[product:ably-tfl/tube]tube:northern:940GZZLUEUS:arrivals';
   channel = this.ably.channels.get(this.channelName);
 
   ngOnInit(): void {
     this.channel.subscribe((msg) => {
       /* station update in msg */
+      this.data = msg.data;
       console.log(msg.data);
     });
   }
