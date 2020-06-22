1dfb77e6abe7b9150439424fbb122c3af6c88537
John Michael Aro <mikoaro1212@gmail.com>
2020-06-22 21:53:54 +0800

    Add subscription to real time data

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
+declare var Ably: any;
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'bolton-test';
+
+  ably = new Ably.Realtime('ZoVHGw.4kc1eQ:xNiIHH2-oeOYIOwG');
+  channelName = '[product:ably-tfl/tube]tube:northern:940GZZLUEUS:arrivals';
+  channel = this.ably.channels.get(this.channelName);
+
+  ngOnInit(): void {
+    this.channel.subscribe((msg) => {
+      /* station update in msg */
+      console.log(msg);
+      console.log(msg.data);
+    });
+  }
 }
